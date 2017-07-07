a6bee780dad2c04a41e8438755d730445ceeb83f
kkli28 <likaixin28@outlook.com>
2017-07-07 23:29:33 +0800

    update

--- Puppy/my_pair.h
+++ Puppy/my_pair.h
@@ -1,12 +1,10 @@
 #pragma once
 
-#pragma once
-
 #include "stdafx.h"
 
 namespace kkli {
 
-	//class my_pair
+	// my_pair类的定义
 	template<typename T1, typename T2>
 	class my_pair {
 	public:
@@ -27,7 +25,7 @@ namespace kkli {
 		}
 
 		template<typename K1, typename K2>
-		my_pair(K1&& f, K2&& s) : first(std::move(f)), second(std::move(s)) {
+		my_pair(K1&& f, K2&& s) : first(f), second(s) {
 			std::cout << "my_pair(K1&&, K2&&)" << std::endl;
 		}
 
@@ -44,7 +42,7 @@ namespace kkli {
 		// operator =
 		template<typename K1, typename K2>
 		my_pair& operator=(const my_pair<K1, K2>& mp) {
-			std::cout << "operator=(const my_pair<K1,K2>&)" << endl;
+			std::cout << "operator=(const my_pair<K1,K2>&)" << std::endl;
 
 			first = mp.first;
 			second = mp.second;
@@ -53,7 +51,7 @@ namespace kkli {
 
 		template<typename K1, typename K2>
 		my_pair& operator=(my_pair<K1,K2>&& mp) {
-			std::cout << "operator=(my_pair<K1,K2>&&)" << endl;
+			std::cout << "operator=(my_pair<K1,K2>&&)" << std::endl;
 
 			first = std::move(mp.first);
 			second = std::move(mp.second);
@@ -63,26 +61,30 @@ namespace kkli {
 		// swap
 		template<typename K1, typename K2>
 		void swap(my_pair<K1, K2>& mp) {
-			std::cout << "swap(my_pair<K1,K2>&)" << endl;
+			std::cout << "swap(my_pair<K1,K2>&)" << std::endl;
 
 			std::swap(first, mp.first);
 			std::swap(second, mp.second);
 		}
 	};
 
-	// ********** non-member functions **********
+	
+}
+
+// ********** non-member functions **********
+namespace kkli{
 
 	// make_pair
 	template<typename T1, typename T2>
 	my_pair<T1, T2> make_pair(const T1& f, const T2& s) {
-		std::cout << "make_pair(const T1&, const T2&)" << endl;
+		std::cout << "make_pair(const T1&, const T2&)" << std::endl;
 		return my_pair(f, s);
 	}
 
 	template<typename T1, typename T2>
 	my_pair<T1, T2> make_pair(T1&& f, T2&& s) {
-		std::cout << "make_pair(T1&&, T2&&)" << endl;
-		return my_pair(f, s);
+		std::cout << "make_pair(T1&&, T2&&)" << std::endl;
+		return my_pair<T1,T2>(f, s);
 	}
 
 	// operator ==
@@ -128,43 +130,42 @@ namespace kkli {
 	void swap(const my_pair<T1, T2>& lhs, const my_pair<T1, T2>& rhs) {
 		lhs.swap(rhs);
 	}
-}
 
-namespace kkli{
+	// get函数模板 与其有关的tuple_element的定义
 
-	//tuple_element<N,my_pair<T1,T2>>
-	template<size_t N, typename T>
+	// class: tuple_element
+	template<size_t N,typename T>
 	class tuple_element;
 
-	//模板偏特化
-	template<typename T1, typename T2>
+	// class: tuple_element<0>
+	template<typename T1,typename T2>
 	class tuple_element<0, my_pair<T1, T2>> {
 	public:
 		typedef typename my_pair<T1, T2>::first_type type;
 	};
 
-	//模板偏特化
-	template<typename T1, typename T2>
+	// class: tuple_element<1>
+	template<typename T1,typename T2>
 	class tuple_element<1, my_pair<T1, T2>> {
 	public:
 		typedef typename my_pair<T1, T2>::second_type type;
 	};
-
+	
 	// get
 	template<size_t N,typename T1,typename T2>
-	typename tuple_element<N,my_pair<T1,T2>>::type& get(my_pair<T1,T2>& mp) {
-		return get(mp, typename tuple_element<N, my_pair<T1, T2>>::type());
+	typename tuple_element<N,my_pair<T1,T2>>::type& get(my_pair<T1, T2>& mp) {
+		return get(mp, typename tuple_element<N,my_pair<T1,T2>>::type());
 	}
 
+	// get<0>
 	template<typename T1,typename T2>
-	typename my_pair<T1, T2>::first_type& get(my_pair<T1, T2>& mp, 
-		typename tuple_element<0, my_pair<T1, T2>>::type) {
+	typename my_pair<T1,T2>::first_type& get(my_pair<T1, T2>& mp, typename tuple_element<0,my_pair<T1,T2>>::type) {
 		return mp.first;
 	}
 
+	// get<1>
 	template<typename T1,typename T2>
-	typename my_pair<T1,T2>::second_type& get(my_pair<T1, T2>& mp, 
-		typename tuple_element<1, my_pair<T1, T2>>::type) {
+	typename my_pair<T1,T2>::second_type& get(my_pair<T1, T2>& mp, typename tuple_element<1,my_pair<T1,T2>>::type) {
 		return mp.second;
 	}
 
--- Puppy/my_pair_test.h
+++ Puppy/my_pair_test.h
@@ -11,23 +11,167 @@ namespace test {
 
 		using kkli::my_pair;
 		using kkli::get;
+		using kkli::make_pair;
+
+		int i = 1;
+		string str = "C++";
+
+		//函数前置声明
+		void test_constructor();
+		void test_types();
+		void test_op_equal();
+		void test_swap();
+		void test_make_pair();
+		void test_op_logical();
+		void test_get();
 
 		//整体测试
 		void test() {
+			cout << "========== test: my_pair ==========" << endl;
 
-			//test: my_pair()
+			test_constructor();
+			test_types();
+			test_op_equal();
+			test_swap();
+			test_make_pair();
+			test_op_logical();
+			test_get();
+		}
+
+		//测试 my_pair
+		void test_constructor() {
+			cout << "\ntest_constructor()" << endl;
+
+			//my_pair()
 			my_pair<int, int> mp1;
 
-			//test: my_pair<T1&&, T2&&>
-			my_pair<int, int> mp2(1, 2);
+			//my_pair<const T1&, const T2&>
 
-			//test: my_pair<const T1&, const T2&>
-			int i = 1;
-			string str = "C++";
-			my_pair<int, string> mp3(i, ref(str));
-			cout << mp3.first << "  " << mp3.second << endl;
-			cout << get<0>(mp3) << endl;
-			cout << get<1>(mp3) << endl;
+			//WRONG: 为何运行后全部都是my_pair<T1&&, T2&&> ???
+			WRONG !!!!!!;
+
+			int ival = 1;
+			string sval = "C++";
+			my_pair<int, string> mp2(ival, ref(sval));
+			my_pair<int, string> mp3 = mp2;
+
+			//my_pair<T1&&, T2&&>
+			my_pair<int, int> mp4(1, 2);
+			my_pair<int, string> mp5(1, "mp3");
+			my_pair<string, int> mp6("mp4", 2);
+			my_pair<int, string> mp7(std::move(ival), std::move(sval));
+		}
+
+		//测试 first_type/second_type/first/second
+		void test_types() {
+			cout << "\ntest_types()" << endl;
+
+			//first_type/second_type
+			
+			//first/second
+			my_pair<int, string> mp(i, str);
+			my_pair<int, string>::first_type ft = i;
+			my_pair<int, string>::second_type st = str;
+			
+			cout << "ft: " << i << "    st: " << st << endl;
+			cout << "first: " << mp.first << "    second: " << mp.second << endl;
+		}
+
+		//测试 operator=
+		void test_op_equal() {
+			cout << "\ntest_op_equal()" << endl;
+
+			//operator=
+			my_pair<int, string> mp1(i, str);
+			my_pair<int, string> mp2;
+			mp2 = mp1;
+			cout << "mp2.first: " << mp2.first << "    mp2.second: " << mp2.second << endl;
+		}
+		
+		//测试 member swap/non-member swap
+		void test_swap() {
+			cout << "\ntest_swap()" << endl;
+
+			my_pair<int, string> mp1(i, str);
+			my_pair<int, string> mp2(2,"C#");
+
+			mp1.swap(mp2);
+			cout << "mp1.first: " << mp1.first << "    mp1.second: " << mp1.second << endl;
+			cout << "mp2.first: " << mp2.first << "    mp2.second: " << mp2.second << endl;
+
+			swap(mp1, mp2);
+			cout << "mp1.first: " << mp1.first << "    mp1.second: " << mp1.second << endl;
+			cout << "mp2.first: " << mp2.first << "    mp2.second: " << mp2.second << endl;
+		}
+
+		//测试 make_pair
+		void test_make_pair() {
+			cout << "\ntest_make_pair()" << endl;
+
+			my_pair<int, string> mp = kkli::make_pair(i, str);
+			cout << "mp.first: " << mp.first << "    mp.second: " << mp.second << endl;
+		}
+
+		//测试 operator < <= > >= == !=
+		void test_op_logical() {
+			cout << "\ntest_op_logical()" << endl;
+
+			my_pair<int, string> mp1(i, str);
+			my_pair<int, string> mp2(i, str);
+			my_pair<int, string> mp3(i, str + "C++");
+			my_pair<int, int> mp4(1, 2);
+			my_pair<int, int> mp5(3, 4);
+
+			//operator ==
+			cout << (mp1 == mp1 ? "==" : "!=") << endl;
+			cout << (mp1 == mp2 ? "==" : "!=") << endl;
+			cout << (mp1 == mp3 ? "==" : "!=") << endl;
+			cout << (mp4 == mp4 ? "==" : "!=") << endl;
+			cout << (mp4 == mp5 ? "==" : "!=") << endl << endl;
+
+			//operator !=
+			cout << (mp1 != mp1 ? "!=" : "==") << endl;
+			cout << (mp1 != mp2 ? "!=" : "==") << endl;
+			cout << (mp1 != mp3 ? "!=" : "==") << endl;
+			cout << (mp4 != mp4 ? "!=" : "==") << endl;
+			cout << (mp4 != mp5 ? "!=" : "==") << endl << endl;
+
+			//operator <
+			cout << (mp1 < mp1 ? "<" : ">=") << endl;
+			cout << (mp1 < mp2 ? "<" : ">=") << endl;
+			cout << (mp1 < mp3 ? "<" : ">=") << endl;
+			cout << (mp4 < mp4 ? "<" : ">=") << endl;
+			cout << (mp4 < mp5 ? "<" : ">=") << endl << endl;
+
+			//operator <=
+			cout << (mp1 <= mp1 ? "<=" : ">") << endl;
+			cout << (mp1 <= mp2 ? "<=" : ">") << endl;
+			cout << (mp1 <= mp3 ? "<=" : ">") << endl;
+			cout << (mp4 <= mp4 ? "<=" : ">") << endl;
+			cout << (mp4 <= mp5 ? "<=" : ">") << endl << endl;
+
+			//operator >
+			cout << (mp1 > mp1 ? ">" : "<=") << endl;
+			cout << (mp1 > mp2 ? ">" : "<=") << endl;
+			cout << (mp1 > mp3 ? ">" : "<=") << endl;
+			cout << (mp4 > mp4 ? ">" : "<=") << endl;
+			cout << (mp4 > mp5 ? ">" : "<=") << endl << endl;
+
+			//operator >=
+			cout << (mp1 >= mp1 ? ">=" : "<") << endl;
+			cout << (mp1 >= mp2 ? ">=" : "<") << endl;
+			cout << (mp1 >= mp3 ? ">=" : "<") << endl;
+			cout << (mp4 >= mp4 ? ">=" : "<") << endl;
+			cout << (mp4 >= mp5 ? ">=" : "<") << endl << endl;
+		}
+
+		//测试 get
+		void test_get() {
+			cout << "\ntest_get()" << endl;
+
+			my_pair<int, string> mp(i, str);
+			cout << "get<0>: " << get<0>(mp) << endl;
+			cout << "get<1>: " << get<1>(mp) << endl;
 		}
 	}
 }
