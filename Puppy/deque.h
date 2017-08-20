c23890173086b300f8978281c361a80badb19716
kkli28 <likaixin28@outlook.com>
2017-08-20 21:18:12 +0800

    re-write __deque_iterator

--- Puppy/deque.h
+++ Puppy/deque.h
@@ -10,47 +10,38 @@
 
 namespace kkli {
 
-	template<typename T, typename Ref, typename Ptr, std::size_t BufSize>
+	template<typename T,typename Ref, typename Ptr, std::size_t BufSize>
 	class __deque_iterator {
 	public:
-		typedef T				value_type;
-		typedef Ptr				pointer;
-		typedef Ref				reference;
-		typedef std::size_t		size_type;
-		typedef std::ptrdiff_t	difference_type;
-		typedef kkli::random_access_iterator_tag	iterator_category;
-
-	public:
+		typedef T	value_type;
+		typedef Ref reference;
+		typedef Ptr pointer;
+		typedef std::size_t size_type;
+		typedef std::ptrdiff_t difference_type;
+		typedef kkli::random_access_iterator_tag iterator_category;
 		typedef T** map_pointer;
+		
+		typedef __deque_iterator self;
 		typedef __deque_iterator<T, T&, T*, BufSize> iterator;
 		typedef __deque_iterator<T, const T&, const T*, BufSize> const_iterator;
-		typedef __deque_iterator self;
-		pointer first;
-		pointer curr;
-		pointer last;
-		map_pointer node;
-		static std::size_t buffer_size() { return BufSize > 8 ? BufSize : 8; } //至少为8
 
-	private:
-		//set_node
-		void set_node(map_pointer new_node) { //跳缓冲区
+	public:
+		T* first;
+		T* curr;
+		T* last;
+		map_pointer node;
+
+		static size_type buffer_size() { return BufSize > 8 ? BufSize : 8; }
+		void set_node(map_pointer new_node) {
 			node = new_node;
 			first = *node;
 			last = first + difference_type(buffer_size());
 		}
-
-		//constructor
-		__deque_iterator() :first(nullptr), curr(nullptr), last(nullptr), node(nullptr) {}
-		__deque_iterator(pointer ptr, map_pointer mp) 
-			:first(*mp), curr(ptr), last(first + difference_type(buffer_size())), node(mp) {}
-		__deque_iterator(const iterator& rhs)
-			:first(rhs.first), curr(rhs.curr), last(rhs.last), node(rhs.node) {}
-
 		reference operator*()const { return *curr; }
 		pointer operator->()const { return &(operator*()); }
-		difference_type operator-(const self& rhs) {
-			return difference_type(buffer_size())*(node - rhs.node - 1) +
-				(curr - first) + (rhs.last - rhs.curr);
+		difference_type operator-(const self& rhs)const {
+			return difference_type(buffer_size())*(node - rhs.node - 1)
+				+ (curr - first) + (rhs.last - rhs.curr);
 		}
 		self& operator++() {
 			++curr;
@@ -79,29 +70,46 @@ namespace kkli {
 			return temp;
 		}
 		self& operator+=(difference_type n) {
-			//TODO: 
+			difference_type offset = (curr - first) + n;
+			//目标位置在当前缓冲区
+			if (offset >= 0 && offset < difference_type(buffer_size())) {
+				curr += n;
+			}
+			//目标位置在其他缓冲区
+			else {
+				/*
+				若offset为正，则可直接处buffer_size获得新node相对于当前node的偏移
+				若offset为负，node_offset则自带一个偏移（因向前，故最前方有负号），故最后需要+1。
+				因offset与buffer_size正好相等时刚好落在first上，此时node_offset不需+1，故
+				需要对-offset进行-1来避免
+				*/
+				difference_type node_offset = offset > 0 ?
+					offset / difference_type(buffer_size())
+					: -(difference_type(-offset - 1) / difference_type(buffer_size()) + 1);
+				set_node(node + node_offset);
+
+				//设置curr在新缓冲区中的位置。注意！offset=(curr-first)+n，即从first开始算起而非curr
+				curr = first + (offset - node_offset*difference_type(buffer_size()));
+			}
+			return *this;
 		}
-		self operator+(difference_type n)const {
+		self operator+(difference_type n) {
 			self temp = *this;
 			return temp += n;
 		}
 		self& operator-=(difference_type n) {
 			return *this += -n;
 		}
-		self operator-(difference_type n)const {
+		self operator-(difference_type n) {
 			self temp = *this;
 			return temp -= n;
 		}
-
 		reference operator[](difference_type n)const {
 			return *(*this + n);
 		}
-		bool operator==(const self& rhs)const {
-			return curr == x.curr;
-		}
-		bool operator!=(const self& rhs)const {
-			return !(*this == rhs);
-		}
+		
+		bool operator==(const self& rhs)const { return curr = rhs.curr; }
+		bool operator!=(const self& rhs)const { return !(*this == rhs); }
 		bool operator<(const self& rhs)const {
 			return (node == rhs.node) ? (curr < rhs.curr) : (node < rhs.node);
 		}
