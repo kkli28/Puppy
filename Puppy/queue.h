e5eb6d79e5abee38a12a475390fe663541402c25
kkli28 <likaixin28@outlook.com>
2017-08-21 21:58:34 +0800

    update deque.h

--- Puppy/queue.h
+++ Puppy/queue.h
@@ -4,48 +4,51 @@
 #include "deque.h"
 
 //================================================================================
-// stack<T> 类定义
+// queue<T> 类定义
 //================================================================================
 
 namespace kkli {
 
 	template<typename T,typename Container=kkli::deque<T>>
-	class stack {
+	class queue {
 	public:
 		typedef Container	container_type;
 		typedef T			value_type;
+		typedef typename Container::size_type		size_type;
 		typedef typename Container::reference		reference;
 		typedef typename Container::const_reference const_reference;
-		typedef typename Container::size_type		size_type;
 
 	private:
 		container_type cont; //内部容器
 
-	public: 
+	public:
 		//constructor
-		explicit stack(const container_type& c) :cont(c) {}
-		explicit stack(container_type&& c = container_type()) :cont(c) { }
-		stack(stack&& rhs) :cont(std::move(rhs.cont)) {}
+		explicit queue(const container_type& c) :cont(c) {}
+		explicit queue(container_type&& c = container_type()) :cont(std::move(c)) {}
+		queue(const queue& rhs) :cont(rhs.cont) {}
+		queue(queue&& rhs) :cont(std::move(rhs.cont)) {}
 
 		//operator =
-		stack& operator=(const stack& rhs) {
+		queue& operator=(const queue& rhs) {
 			cont = rhs.cont;
 			return *this;
 		}
-		stack& operator=(stack&& rhs) {
+		queue& operator=(queue&& rhs) {
 			cont = std::move(rhs.cont);
 			return *this;
 		}
 
 		const container_type& get_container()const { return cont; }
-		reference top() { return cont.back(); }
-		const_reference top()const { return cont.back(); }
+		reference front() { return cont.front(); }
+		const_reference front()const { return cont.front(); }
+		reference back() { return cont.back(); }
+		const_reference back()const { return cont.back(); }
 		bool empty()const { return cont.empty(); }
 		size_type size()const { return cont.size(); }
 		void push(const value_type& value) { cont.push_back(value); }
 		void push(value_type&& value) { cont.push_back(std::move(value)); }
-		void pop() { cont.pop_back(); }
-		void swap(stack& rhs) {
+		void pop() { cont.pop_front(); }
+		void swap(deque& rhs) {
 			kkli::swap(cont, rhs.cont);
 		}
 	};
@@ -58,44 +61,38 @@ namespace kkli {
 namespace kkli {
 
 	//operator ==
-	template<typename T, typename Container>
-	bool operator==( stack<T, Container>& lhs,  stack<T, Container>& rhs) {
+	template<typename T,typename Container>
+	bool operator==(const deque<T, Container>& lhs, const deque<T, Container>& rhs) {
 		return lhs.get_container() == rhs.get_container();
 	}
-	
+
 	//operator !=
 	template<typename T, typename Container>
-	bool operator!=( stack<T, Container>& lhs,  stack<T, Container>& rhs) {
+	bool operator!=(const deque<T, Container>& lhs, const deque<T, Container>& rhs) {
 		return !(lhs == rhs);
 	}
 
 	//operator <
 	template<typename T, typename Container>
-	bool operator<( stack<T, Container>& lhs,  stack<T, Container>& rhs) {
+	bool operator<(const deque<T, Container>& lhs, const deque<T, Container>& rhs) {
 		return lhs.get_container() < rhs.get_container();
 	}
 
 	//operator >
 	template<typename T, typename Container>
-	bool operator>( stack<T, Container>& lhs,  stack<T, Container>& rhs) {
+	bool operator>(const deque<T, Container>& lhs, const deque<T, Container>& rhs) {
 		return rhs < lhs;
 	}
 
 	//operator <=
 	template<typename T, typename Container>
-	bool operator<=( stack<T, Container>& lhs,  stack<T, Container>& rhs) {
+	bool operator<=(const deque<T, Container>& lhs, const deque<T, Container>& rhs) {
 		return !(lhs > rhs);
 	}
 
 	//operator >=
 	template<typename T, typename Container>
-	bool operator>=( stack<T, Container>& lhs,  stack<T, Container>& rhs) {
+	bool operator>=(const deque<T, Container>& lhs, const deque<T, Container>& rhs) {
 		return !(lhs < rhs);
-	}
-
-	//swap
-	template<typename T, typename Container>
-	void swap(stack<T,Container>& lhs, stack<T, Container>& rhs) {
-		return lhs.swap(rhs);
 	}
 }
